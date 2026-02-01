e177475cd80f53de41010803998178ae0e3c92cb
Palak Jain <palakjain@Palaks-MacBook-Air.local>
2026-02-01 19:57:43 +0530

    Prepare frontend for production

--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
 // API Configuration
 const API_BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
-export const API_ENDPOINTS = {
+const API_ENDPOINTS = {
   INDICATORS: `${API_BASE_URL}/metadata/indicators`,
   APPLY_FILTERS: `${API_BASE_URL}/filters/apply`,
 };
 
-export default {
+const config = {
   API_BASE_URL,
   API_ENDPOINTS,
 };
+
+export default config;
+export { API_BASE_URL, API_ENDPOINTS };
